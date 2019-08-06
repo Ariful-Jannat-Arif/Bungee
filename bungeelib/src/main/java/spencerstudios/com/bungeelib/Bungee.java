2e5cfb50582c4d8a6f6720108c48bfc1cdaa3653
Ariful Jannat Arif <31162390+Ariful-Jannat-Arif@users.noreply.github.com>
2019-08-06 12:29:53 +0600

    Update Bungee.java

--- bungeelib/src/main/java/spencerstudios/com/bungeelib/Bungee.java
+++ bungeelib/src/main/java/spencerstudios/com/bungeelib/Bungee.java
@@ -3,7 +3,7 @@ package spencerstudios.com.bungeelib;
 import android.app.Activity;
 import android.content.Context;
 
-public class Bungee {
+public class Animations{
 
     public static void slideLeft(Context context) {
         ((Activity) context).overridePendingTransition(R.anim.slide_left_enter, R.anim.slide_left_exit);
